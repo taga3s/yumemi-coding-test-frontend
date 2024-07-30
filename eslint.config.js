aa6c63f05189e5339a5f634eccf97701c67d8a36
taga3s <bepozurapu274@gmail.com>
2024-07-30 21:10:24 +0900

    chore: fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,37 +1,75 @@
-import eslint from "@eslint/js";
-import simpleImportSort from "eslint-plugin-simple-import-sort";
-import globals from "globals"
-import tseslint from "typescript-eslint";
+import globals from 'globals';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import pluginJsxA11y from 'eslint-plugin-jsx-a11y';
+import pluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import { fixupPluginRules } from '@eslint/compat';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 
-
-export default [
+export default tseslint.config(
+  {
+    ignores: ['node_modules', '.env'],
+  },
   eslint.configs.recommended,
+  pluginPrettierRecommended,
   ...tseslint.configs.recommended,
   {
     languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-      globals: {
-        ...globals.browser,
-        ...globals.es2021,
-        ...globals.node,
-      },
+      ecmaVersion: 'latest',
       parserOptions: {
         ecmaFeatures: {
-            jsx: true
-        }
-      }
+          jsx: true,
+        },
+      },
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.commonjs,
+        ...globals.es2015,
+      },
     },
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
     plugins: {
-      "simple-import-sort": simpleImportSort,
+      react: pluginReact,
+      'jsx-a11y': pluginJsxA11y,
+      'react-hooks': fixupPluginRules(pluginReactHooks),
+      'simple-import-sort': simpleImportSort,
     },
     rules: {
-      "simple-import-sort/imports": "error",
-      "simple-import-sort/exports": "error",
+      ...pluginReact.configs.recommended.rules,
+      ...pluginReact.configs['jsx-runtime'].rules,
+      ...pluginJsxA11y.configs.recommended.rules,
+      ...pluginReactHooks.configs.recommended.rules,
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
     },
-    ignores: [
-      "node_modules",
-      "dist",
-    ]
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  {
+    files: ['eslint.config.js'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-var-requires': 'error',
+      '@typescript-eslint/consistent-type-assertions': 'error',
+      'no-implicit-coercion': 'error',
+      'no-console': 'error',
+      'prefer-template': 'error',
+    },
   }
-]
+);
