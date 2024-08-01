db1876e0e97fa418df4c2dcef6b4d73f81799000
taga3s <bepozurapu274@gmail.com>
2024-08-01 13:59:30 +0900

    refac: selectbox

--- src/features/Statistics/components/StatisticsLineGraph.tsx
+++ src/features/Statistics/components/StatisticsLineGraph.tsx
@@ -4,7 +4,8 @@ import { type FC } from 'react';
 
 import { buildLabels, buildOptions } from './funcs/graphBuilder';
 import { PopulationData } from './StatisticsContainer';
-import { statisticsLineGraphLayout, statisticsLineGraphSelectBox, statisticsLineGraphSelectBoxInner } from './StatisticsLineGraph.css';
+import { statisticsLineGraphLayout } from './StatisticsLineGraph.css';
+import { StatisticsLineGraphSelectbox } from './StatisticsLineGraphSelectbox';
 
 type Props = {
   populationData: PopulationData;
@@ -23,21 +24,11 @@ const StatisticsLineGraph: FC<Props> = (props) => {
         <span>都道府県を選択するとグラフが表示されます</span>
       ) : (
         <>
-          <label className={statisticsLineGraphSelectBox} htmlFor='population-data-range'>
-            人口範囲を選択します
-            <select
-              className={statisticsLineGraphSelectBoxInner}
-              id='population-data-range'
-              onChange={(e) => onChangeSelectedPopulationDataLabel(e.target.value)}
-              defaultValue={selectedPopulationDataLabel}
-            >
-              {populationDataLabels.map((label) => (
-                <option value={label} key={label}>
-                  {label}
-                </option>
-              ))}
-            </select>
-          </label>
+          <StatisticsLineGraphSelectbox
+            selectedPopulationDataLabel={selectedPopulationDataLabel}
+            populationDataLabels={populationDataLabels}
+            onChangeSelectedPopulationDataLabel={onChangeSelectedPopulationDataLabel}
+          />
           <HighchartsReact highcharts={Highcharts} options={buildOptions(populationData, selectedPopulationDataLabel)} />
         </>
       )}
